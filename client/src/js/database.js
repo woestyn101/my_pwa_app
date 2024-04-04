f5c0f7c1d8b6c84336a2299baa2d1eac5c134456
Marius Du Plooy <woestyn101@gmail.com>
2024-04-03 22:23:27 -0400

    install and database set

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+const contactDb = await openDB('jate', 1);  
+
+const tx = contactDb.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({ id: 1, value: content });
+const result = await request;
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  
+    
+    const contactDb = await openDB('jate', 1);   
+    const tx = contactDb.transaction('jate', 'readonly');      
+    const store = tx.objectStore('jate');   
+    const request = store.getAll(); 
+   
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+};
 
 initdb();
