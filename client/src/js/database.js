aa9d2d6b98cf2f6ed2e3b567485849062ffe7a37
Marius Du Plooy <woestyn101@gmail.com>
2024-04-04 10:18:29 -0400

    service worker fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,12 @@
 import { openDB } from 'idb';
 
+const indexedDB =
+   window.indexedDB ||
+   window.mozIndexedDB ||
+   window.webkitIndexedDB ||
+   window.msIndexedDB ||
+   window.shimIndexedDB;
+
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -14,7 +21,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-// create connection to db
+// create connection to db with database name and database version number
 const contactDb = await openDB('jate', 1);  
 // make a new transaction wiht options
 const tx = contactDb.transaction('jate', 'readwrite');
@@ -23,6 +30,18 @@ const store = tx.objectStore('jate');
 const request = store.put({ id: 1, value: content });
 const result = await request;
 
+    request.onsuccess = function (){
+      //document.write("database created")
+      console.log("Database created");
+    }
+    request.onerror = function(event){
+      document.write("Error: Database not created " + event.target.errorCode);
+    }
+
+    tx.oncomplete = function(){
+      db.close;
+   }
+
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -30,13 +49,21 @@ export const getDb = async () => {
   
     
     const contactDb = await openDB('jate', 1);   
-    const tx = contactDb.transaction('jate', 'readonly');      
+
+    // create a transaction to interact with the database
+    const tx = contactDb.transaction('jate', 'readonly');   
+    
+    // createing an objectstore to store the data in the database
     const store = tx.objectStore('jate');   
     const request = store.getAll(); 
    
     const result = await request;
     console.log('result.value', result);
     return result?.value;
+
+    
+
+    
 };
 
 initdb();
